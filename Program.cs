07f6961134a4dea43de28fb7bb65517d79550d7e
ETamez173 <56414835+ETamez173@users.noreply.github.com>
2021-03-08 16:05:05 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharpExercises_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello Dog!");
+
+            //Console.WriteLine("    /|");
+            //Console.WriteLine("   / |");
+            //Console.WriteLine("  /  |");
+            //Console.WriteLine(" /   |");
+            //Console.WriteLine("/____|");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
